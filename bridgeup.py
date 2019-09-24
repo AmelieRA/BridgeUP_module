e8a8ead91cbf5e2af917204331c4847380dcfb4a
Rie A <riejazz27@gmail.com>
2019-09-24 17:46:56 -0400

    gygft

--- bridgeup.py
+++ bridgeup.py
@@ -4,5 +4,13 @@
 # and then add your function below
 
 import pandas as pd
-
 df = pd.read_csv("internship_bootcamp_data.csv")
+
+
+# taking student name and returning animal
+def student_animal(name):
+	student = df["First Name"]
+	animal = df["Fav animal"]
+	student_info = animal.loc[student == name]
+	return student_info
+
